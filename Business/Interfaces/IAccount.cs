24be6227f639b1fce091a9c3bc3c81228d852045
Olga Yaroshchuk <yrox152@gmail.com>
2017-02-03 01:43:38 +0300

    IContacts, IMessaging removed

--- Business/Interfaces/IAccount.cs
+++ Business/Interfaces/IAccount.cs
@@ -1,9 +1,18 @@
-﻿namespace Business.Interfaces
+﻿using System.Collections.Generic;
+using DTOs;
+
+namespace Business.Interfaces
 {
-    public interface IAccount : IContacts, IMessaging
-    {
+    public interface IAccount
+    { 
         void Authorize(string code);
         void Authorize(string captcha, long sid);
-        
+
+        IEnumerable<ContactDTO> GetAllContacts();
+        void NewContactAdded(ContactDTO contact);
+
+        void SendMessage(MessageDTO message);
+        void SendMessage(MessageDTO message, string captcha, long sid);
+
     }
 }
