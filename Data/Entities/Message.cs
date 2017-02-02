2b3e204b557da9a53d08501da86a168f2e2ffcd4
Olga Yaroshchuk <yrox152@gmail.com>
2017-02-02 11:37:10 +0300

    message entity added

---
+++ Data/Entities/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace Data.Entities
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public int MetaContactId { get; set; }
+    }
+}
