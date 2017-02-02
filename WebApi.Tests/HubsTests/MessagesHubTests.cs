556022986a32cc68e4bcb28f96b2a95b24735a3b
Olga Yaroshchuk <yrox152@gmail.com>
2017-02-02 12:57:54 +0300

    MessagesHubTests

---
+++ WebApi.Tests/HubsTests/MessagesHubTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNet.SignalR;
+using NUnit.Framework;
+using WebApi.Hubs;
+
+namespace WebApi.Test.HubsTests
+{
+    [TestFixture]
+    public class MessagesHubTests
+    {
+        [Test]
+        public void NewConactAdded_ShouldNotifyClient_Succeed()
+        {
+            var hub = new Lazy<IHubContext>(
+            () => GlobalHost.ConnectionManager.GetHubContext<MessagesHub>()
+        );
+            hub.Value.Clients.All.NewContactAdded();
+            Assert.That(hub.IsValueCreated);
+        }
+    }
+}
