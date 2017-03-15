5ace0751d3c9266f0931e09c56cd9abb43b5860c
Olga Yaroshchuk <yrox152@gmail.com>
2017-03-15 11:55:30 +0300

    "Text" prop added

--- Dialog.Business/Dialog.Business.DTO/MessageDTO.cs
+++ Dialog.Business/Dialog.Business.DTO/MessageDTO.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public int AccountId { get; set; }
+        public string Text { get; set; }
         public int MetaContactId { get; set; }
         public string MetaContactIName { get; set; }
     }
