9578b2316922e3c4b5e107c2bb3897b8d02bac30
Olga Yaroshchuk <yrox152@gmail.com>
2017-04-20 23:12:26 +0300

    dateTime vk converting

--- Dialog.Business/Dialog.Business.Accounts/Util/VkUpdatesParser.cs
+++ Dialog.Business/Dialog.Business.Accounts/Util/VkUpdatesParser.cs
@@ -14,7 +14,6 @@ namespace Dialog.Business.Accounts.Util
             return JsonConvert.DeserializeObject<VkLongPollingUpdates>(updates);
         }
 
-        //TODO converting from unix timestamp to datetime
         public static IEnumerable<VkNet.Model.Message> GetMessagesFromUpdate(VkLongPollingUpdates updates)
         {
             
@@ -22,7 +21,7 @@ namespace Dialog.Business.Accounts.Util
             return newMessages.Select(message => message as IList<string> ?? message.ToList()).Select(enumerable => new Message
             {
                 Body = enumerable.ElementAt(6),
-                //Date = EntitiesMapper.ConvertFromUnixTimestamp(Convert.ToInt32(enumerable.ElementAt(4))),
+                Date = DateTimeOffset.FromUnixTimeMilliseconds(Convert.ToInt64(enumerable.ElementAt(4))).UtcDateTime,
                 FromId = Convert.ToInt32(enumerable.ElementAt(3))
             }).ToList();
         }
