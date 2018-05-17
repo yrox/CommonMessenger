71b533f042df8ceb4e99c8121e172b28f5d7605a
Olga Yaroshchuk <yrox152@gmail.com>
2018-05-17 10:09:52 +0300

    Message components implementation

--- Dialog.WebClient/dialog-web-client/src/components/message/message.js
+++ Dialog.WebClient/dialog-web-client/src/components/message/message.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
+import _ from "lodash";
+import classNames from "classnames";
 
 export default class Message extends Component {
+  renderMessage(message) {
+    const text = message.text || '';
+
+    const messageElement = _.split(text, "\n").map((m, key) => {
+      return <p key={key} dangerouslySetInnerHTML={{ __html: m }} />;
+    });
+
+    return messageElement;
+  }
+
   render() {
-    const { text, date, author } = this.props;
+    const { index, message } = this.props;
     return (
-      <div>
-        <div>author</div>
-        <div>text</div>
-        <div>date</div>
+      <div key={index} className={classNames("message", { me: message.me })}>
+        <div className="message-user-image">
+          <img alt="pic" />
+        </div>
+        <div className="message-body">
+          <div className="message-author">
+            {message.me ? "You " : message.author} says:
+          </div>
+          <div className="message-text">{this.renderMessage(message)}</div>
+        </div>
       </div>
     );
   }
--- Dialog.WebClient/dialog-web-client/src/components/message/messageList.js
+++ Dialog.WebClient/dialog-web-client/src/components/message/messageList.js
@@ -4,6 +4,12 @@ import Message from "./message";
 export default class MessageList extends Component {
   render() {
     const { messages } = this.props;
-    return <div>{messages.map(mes => <div>{mes}</div>)}</div>;
+    return (
+      <div ref={ref => (this.messagesRef = ref)} className="messages">
+        {messages.map((message, index) => {
+          return <Message index={index} message={message} />;
+        })}
+      </div>
+    );
   }
 }
